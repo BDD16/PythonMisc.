29df9088734678b51a781492d5c912dfb744e1c1
BDG_Admin <blake.degarza@gmail.com>
2021-07-25 13:57:19 -0500

    started fixing the problems section that were being detected by the IDE.

--- 1337_Tech_Blog/_1337_Tech_Blog/NeutrinoKey/decorators.py
+++ 1337_Tech_Blog/_1337_Tech_Blog/NeutrinoKey/decorators.py
@@ -7,11 +7,12 @@ from django.conf import settings
 from django.contrib.auth import get_user
 from django.shortcuts import redirect
 
+
 def custom_login_required(view):
-    #view argument must be a function
+    # view argument must be a function
 
     def new_view(request, *args, **kwargs):
-        #View argument must be a function
+        # View argument must be a function
         user = get_user(request)
         if user.is_authenticated():
             return view(request, *args, **kwargs)
@@ -19,9 +20,7 @@ def custom_login_required(view):
             url = '{}?next={}'.format(settings.LOGIN_URL, request.path)
             return redirect(url)
 
-        return new_view
-
-    #TODO: ADDIN ZEROKNOWLEDGE AUTHENTICATION_WZK Implementation
-    #My Idea to make this authentication work is using an && between django's
-    #Built in authentication and my own ZKA_wzk implementation.  That way it
-    #would take both to fail catastrophically in order for a user to be compromised
+    # TODO: ADD IN ZERO KNOWLEDGE AUTHENTICATION_WZK Implementation
+    # My Idea to make this authentication work is using an && between django's
+    # Built in authentication and my own ZKA_wzk implementation.  That way it
+    # would take both to fail catastrophically in order for a user to be compromised
